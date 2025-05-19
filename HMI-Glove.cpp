22e30c957204c02d09afe56685e355709e969f16
Muhd Syamim <Syazam33@gmail.com>
2025-05-20 04:21:07 +0800

    feat: Add FreeRTOS

--- HMI-Glove.cpp
+++ HMI-Glove.cpp
@@ -1,7 +1,25 @@
+#include <stdio.h>
+
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
-#include <stdio.h>
+// LWiP
+#include "lwipopts.h"
+
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
+bool state = false;
+
+void toggle(void* param) {
+	while (true) {
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
+		state ^= true;
+		vTaskDelay(1000);
+	}
+}
 
 int main() {
 	stdio_init_all();
@@ -12,10 +30,12 @@ int main() {
 		return -1;
 	}
 
-	bool state = false;
-	while (true) {
-		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
-		state ^= true;
-		sleep_ms(1000);
-	}
+	cyw43_arch_enable_sta_mode();
+	if (cyw43_arch_wifi_connect_timeout_ms("HOMER", "92378736", CYW43_AUTH_WPA3_WPA2_AES_PSK, 10000) != PICO_OK) return 0;
+
+	TaskHandle_t ledHandle = NULL;
+
+	uint32_t status = xTaskCreate(toggle, "Toggle LED", 256, NULL, 1, &ledHandle);
+
+	vTaskStartScheduler();
 }
